ed5b1b6f02c504155c972f48772b0bc8e3ed99f6
Miłosz Osiński <mylosz@yahoo.pl>
2012-10-11 21:25:52 +0200

    ver 2 gry

---
+++ Gra.ver2/Gra/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Gra
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.Gracz = new System.Windows.Forms.PictureBox();
+            this.Komputer = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Gracz)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Komputer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(128, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Nick Gracza:";
+            // 
+            // Gracz
+            // 
+            this.Gracz.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Gracz.Image = global::Gra.Properties.Resources.question_md;
+            this.Gracz.InitialImage = null;
+            this.Gracz.Location = new System.Drawing.Point(38, 76);
+            this.Gracz.Name = "Gracz";
+            this.Gracz.Size = new System.Drawing.Size(193, 193);
+            this.Gracz.TabIndex = 3;
+            this.Gracz.TabStop = false;
+            this.Gracz.Click += new System.EventHandler(this.Gracz_Click);
+            // 
+            // Komputer
+            // 
+            this.Komputer.Cursor = System.Windows.Forms.Cursors.WaitCursor;
+            this.Komputer.ErrorImage = null;
+            this.Komputer.Image = global::Gra.Properties.Resources.computer_md;
+            this.Komputer.Location = new System.Drawing.Point(347, 76);
+            this.Komputer.Name = "Komputer";
+            this.Komputer.Size = new System.Drawing.Size(193, 193);
+            this.Komputer.TabIndex = 4;
+            this.Komputer.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(133, 5);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(130, 30);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ".............";
+            this.label2.FontChanged += new System.EventHandler(this.label2_Click);
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(138, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 30);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Visible = false;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.Color.DarkGreen;
+            this.label3.Location = new System.Drawing.Point(343, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(176, 24);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Do ilu wygranych:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(519, 5);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 30);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "..";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox1.Image = global::Gra.Properties.Resources.paper_md;
+            this.pictureBox1.Location = new System.Drawing.Point(38, 55);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(137, 137);
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(219, 55);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(137, 137);
+            this.pictureBox2.TabIndex = 12;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox3.Image = global::Gra.Properties.Resources.rock_md;
+            this.pictureBox3.Location = new System.Drawing.Point(403, 55);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(137, 137);
+            this.pictureBox3.TabIndex = 13;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Visible = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.numericUpDown1.Location = new System.Drawing.Point(514, 2);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(45, 35);
+            this.numericUpDown1.TabIndex = 14;
+            this.numericUpDown1.Visible = false;
+            this.numericUpDown1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.numericUpDown1_KeyDown);
+            // 
+            // label5
+            // 
+            this.label5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label5.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label5.Font = new System.Drawing.Font("Segoe Script", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(240)));
+            this.label5.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label5.Location = new System.Drawing.Point(113, 244);
+            this.label5.Margin = new System.Windows.Forms.Padding(0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(174, 96);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.Font = new System.Drawing.Font("Segoe Script", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(240)));
+            this.label6.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label6.Location = new System.Drawing.Point(253, 244);
+            this.label6.Margin = new System.Windows.Forms.Padding(0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 96);
+            this.label6.TabIndex = 15;
+            this.label6.Text = ":";
+            // 
+            // label7
+            // 
+            this.label7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label7.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label7.Font = new System.Drawing.Font("Segoe Script", 60F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(240)));
+            this.label7.ImageAlign = System.Drawing.ContentAlignment.TopLeft;
+            this.label7.Location = new System.Drawing.Point(364, 244);
+            this.label7.Margin = new System.Windows.Forms.Padding(0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(176, 96);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "0";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox4.Image = global::Gra.Properties.Resources.spock_md;
+            this.pictureBox4.Location = new System.Drawing.Point(138, 203);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(137, 137);
+            this.pictureBox4.TabIndex = 17;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Visible = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.pictureBox5.Image = global::Gra.Properties.Resources.lizard_md;
+            this.pictureBox5.Location = new System.Drawing.Point(327, 203);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(105, 137);
+            this.pictureBox5.TabIndex = 18;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Visible = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(584, 362);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Komputer);
+            this.Controls.Add(this.Gracz);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.95D;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Papier Nożyce Kamień Jaszczurka Spock";
+            ((System.ComponentModel.ISupportInitialize)(this.Gracz)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Komputer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox Gracz;
+        private System.Windows.Forms.PictureBox Komputer;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ Gra.ver2/Gra/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Media;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gra
+{
+    public partial class Form1 : Form
+    {
+        static int x = 0, komputer = 0, czlowiek = 0, wynik_komp = 0, wynik_czlowiek = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            textBox1.Visible = true;
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (textBox1.Text == "")
+                {
+                    label2.Text = ".............";
+                    textBox1.Visible = false;
+                }
+                else
+                {
+                label2.Text = textBox1.Text;
+                textBox1.Visible = false;
+                } 
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                textBox1.Visible = false;     
+            }
+        }
+
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Visible = true;
+        }
+
+        private void Gracz_Click(object sender, EventArgs e)
+        {
+            Random r = new Random();
+            komputer = r.Next(5);
+            if (label2.Text != "............." & label4.Text != "..")
+            {
+                x = Convert.ToInt32(label4.Text);
+                if (wynik_czlowiek < x && wynik_komp < x)
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox2.Visible = true;
+                    pictureBox3.Visible = true;
+                    pictureBox4.Visible = true;
+                    pictureBox5.Visible = true;
+                }
+                else
+                {
+                    if (MessageBox.Show("Czy chcesz zagrać jeszcze raz ?", "Koniec Rozgrywki", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        wynik_komp = 0;
+                        wynik_czlowiek = 0;
+                        label5.Text = Convert.ToString(wynik_czlowiek);
+                        label7.Text = Convert.ToString(wynik_komp);
+                        Gracz.Image = Properties.Resources.question_md;
+                        Komputer.Image = Properties.Resources.computer_md;
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dziękujemy za gre i zapraszamy ponownie!");
+                        Application.Exit();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Najpierw uzupełnij Imie gracza oraz ilość wygranych!");
+            }
+        }
+
+        //reakcja po naciśnieciu odpowiedniego obrazka
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            czlowiek = 1;
+            Gracz.Image = Properties.Resources.scissors_big;
+            silnik();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            czlowiek = 0;
+            Gracz.Image = Properties.Resources.paper_big;
+            silnik();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            czlowiek = 2;
+            Gracz.Image = Properties.Resources.rock_big;
+            silnik();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            czlowiek = 3;
+            Gracz.Image = Properties.Resources.spock_big;
+            silnik();
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            czlowiek = 4;
+            Gracz.Image = Properties.Resources.lizard_big;
+            silnik();
+        }
+
+        private void silnik()
+        {
+            //ukrycie obrazków wyboru
+            pictureBox1.Visible = false;
+            pictureBox2.Visible = false;
+            pictureBox3.Visible = false;
+            pictureBox4.Visible = false;
+            pictureBox5.Visible = false;
+
+            //podniesienie wyniku komputera za każdy wygrany przypadek
+            if ((czlowiek == 0 && komputer == 1) || (czlowiek == 0 && komputer == 4) || (czlowiek == 1 && komputer == 2) || (czlowiek == 1 && komputer == 3) || (czlowiek == 2 && komputer == 0) || (czlowiek == 2 && komputer == 3) || (czlowiek == 3 && komputer == 0) || (czlowiek == 3 && komputer == 4) || (czlowiek == 4 && komputer == 1) || (czlowiek == 4 && komputer == 2))
+            {
+                wynik_komp++;
+                label7.Text = Convert.ToString(wynik_komp);
+            }
+
+            //podniesienie wyniku komputera za każdy wygrany przypadek
+            else if ((czlowiek == 0 && komputer == 2) || (czlowiek == 0 && komputer == 3) || (czlowiek == 1 && komputer == 0) || (czlowiek == 1 && komputer == 4) || (czlowiek == 2 && komputer == 1) || (czlowiek == 2 && komputer == 4) || (czlowiek == 3 && komputer == 1) || (czlowiek == 3 && komputer == 2) || (czlowiek == 4 && komputer == 0) || (czlowiek == 4 && komputer == 3))
+            {
+                wynik_czlowiek++;
+                label5.Text = Convert.ToString(wynik_czlowiek);
+            }
+
+            //przypożadkowanie wylosawanej liczbie komputera odpowiedni obrazek
+            if (komputer == 0)
+            {
+                Komputer.Image = Properties.Resources.paper_big;
+            }
+            else if (komputer == 1)
+            {
+                Komputer.Image = Properties.Resources.scissors_big;
+            }
+            else if (komputer == 2)
+            {
+                Komputer.Image = Properties.Resources.rock_big;
+            }
+            else if (komputer == 3)
+            {
+                Komputer.Image = Properties.Resources.spock_big;
+            }
+            else if (komputer == 4)
+            {
+                Komputer.Image = Properties.Resources.lizard_big;
+            }
+
+            //odtwarzanie dźwięku
+            if ((wynik_czlowiek == x) || (wynik_komp == x))
+            {
+                if (wynik_czlowiek > wynik_komp)
+                {
+                    SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.win);
+                    simpleSound.Play();
+                }
+                else
+                {
+                    SoundPlayer simpleSound = new SoundPlayer(Properties.Resources.lose);
+                    simpleSound.Play();
+                }
+            }
+        }
+
+        private void numericUpDown1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                if (numericUpDown1.Text == "0")
+                {
+                    label4.Text = "..";
+                    numericUpDown1.Visible = false;
+                }
+                else if (numericUpDown1.Text != "0")
+                {
+                    label4.Text = numericUpDown1.Text;
+                    numericUpDown1.Visible = false;
+                }
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                numericUpDown1.Visible = false;
+            }
+        }
+
+        private void s(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
